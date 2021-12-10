2321e0b25956b7c611e8caaeb530631c5e126a61
Litter Ádám <70979140+LitterAdamDev@users.noreply.github.com>
2021-12-10 14:39:52 +0100

    delete domain

--- src/config/base.js
+++ src/config/base.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import "firebase/storage"
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAMGuVAi44K_quisO2jMzY6QjXMI4pgSlg",
-  authDomain: "szema-beta.firebaseapp.com",
-  projectId: "szema-beta",
-  storageBucket: "szema-beta.appspot.com",
-  messagingSenderId: "13580138535",
-  appId: "1:13580138535:web:893e066778947f67e57602",
-  measurementId: "G-BKCWEH5F81"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 const storage = firebase.storage();
